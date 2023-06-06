c339312eed7eae6c95c4fcad9300bdcd888df851
Furkan Memis <fmemis0160@gmail.com>
2023-06-06 23:55:50 +0300

    test example

---
+++ specs/myClass.spec.js
@@ -0,0 +1,13 @@
+var MyClass = require("../src/myClass");
+
+var myObj = new MyClass();
+
+var chai = require("chai");
+var expect = chai.expect;
+
+
+describe("Test suit", function (){
+    it("Test the add method",function (){
+        expect(myObj.add(1,2)).to.be.equal(3);
+    })
+});
